b838aac039066c4a8f9d2731f6bb04f1314ec559
Essi Savonius <essi.savonius@hotmail.fi>
2023-07-14 12:32:59 +0300

    scaling edits

--- js/logic.js
+++ js/logic.js
@@ -1,3 +1,5 @@
+// Hiding 2nd nav-bar when on home page
+
 const navbar = document.getElementById('navbar');
 const homeSection = document.getElementById('home');
 
@@ -16,3 +18,21 @@ function toggleNavbar() {
 navbar.classList.add('hidden');
 
 window.addEventListener('scroll', toggleNavbar);
+
+
+// Scaling nav-bar according to font-size
+
+// Function to scale the home-nav-bar width based on the font size
+function scaleNavWidth() {
+  const fontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
+  const navBar = document.querySelector('.home-nav-bar');
+  const navWidth = fontSize * 0.7; // 2% of the font size
+
+  navBar.style.width = `${navWidth}%`; // Set the width as a percentage
+}
+
+// Call the function when the page loads and on window resize
+window.addEventListener('load', scaleNavWidth);
+window.addEventListener('resize', scaleNavWidth);
+
+
