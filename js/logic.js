32c7d16b0f658a375ba3ac50985d74d8d830f09c
Essi Savonius <essi.savonius@hotmail.fi>
2023-07-14 13:44:09 +0300

    navbar buttons activate

--- js/logic.js
+++ js/logic.js
@@ -1,7 +1,8 @@
-// Hiding 2nd nav-bar when on home page
-
 const navbar = document.getElementById('navbar');
 const homeSection = document.getElementById('home');
+const ulList = document.getElementById('ul-list');
+const navLinks = ulList.querySelectorAll('a');
+const scrollOffset = 100; // Adjust this value as needed
 
 function toggleNavbar() {
   const homeSectionHeight = homeSection.clientHeight;
@@ -9,30 +10,36 @@ function toggleNavbar() {
 
   if (scrollTop >= homeSectionHeight) {
     navbar.classList.remove('hidden');
+    ulList.classList.remove('ul-list-hidden');
+    ulList.style.height = 'auto';
   } else {
     navbar.classList.add('hidden');
+    ulList.classList.add('ul-list-hidden');
+    ulList.style.height = homeSectionHeight + 'px';
+  }
+
+  // Remove active class from all nav links
+  for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].parentNode.classList.remove('active');
+  }
+
+  // Add active class to the nav link corresponding to the current section
+  for (var i = 0; i < navLinks.length; i++) {
+    const sectionId = navLinks[i].getAttribute('href').substring(1);
+    const section = document.getElementById(sectionId);
+    const sectionTop = section.offsetTop - scrollOffset; // Apply the offset
+    const sectionHeight = section.offsetHeight;
+
+    if (scrollTop >= sectionTop && scrollTop < sectionTop + sectionHeight) {
+      navLinks[i].parentNode.classList.add('active');
+    }
   }
 }
 
 // Hide the navbar on page load
 navbar.classList.add('hidden');
+ulList.classList.add('ul-list-hidden');
+ulList.style.height = homeSection.clientHeight + 'px';
 
 window.addEventListener('scroll', toggleNavbar);
-
-
-// Scaling nav-bar according to font-size
-
-// Function to scale the home-nav-bar width based on the font size
-function scaleNavWidth() {
-  const fontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
-  const navBar = document.querySelector('.home-nav-bar');
-  const navWidth = fontSize * 0.7; // 2% of the font size
-
-  navBar.style.width = `${navWidth}%`; // Set the width as a percentage
-}
-
-// Call the function when the page loads and on window resize
-window.addEventListener('load', scaleNavWidth);
-window.addEventListener('resize', scaleNavWidth);
-
-
+window.addEventListener('resize', toggleNavbar);
